ca8a510c10a4e0e7db6143301893ae84224c9050
dhoegl <firebird@hoeglund.com>
2024-02-26 12:07:13 -0800

    NavBar Start Work

---
+++ app/NavBar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <nav>
+        <Link href="/">Logo</Link>
+        <ul>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default NavBar
